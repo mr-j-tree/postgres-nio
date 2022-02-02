56a3d5172b3f567c433e7cf4123d936212f451da
Josh Lokot <josh@lokot.com>
2022-02-02 09:20:10 +0000

    Target iOS v15.

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
     name: "postgres-nio",
     platforms: [
         .macOS(.v10_15),
-        .iOS(.v13),
+        .iOS(.v15),
         .watchOS(.v6),
         .tvOS(.v13),
     ],
